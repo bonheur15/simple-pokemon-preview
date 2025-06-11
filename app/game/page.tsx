c17d6991ae2be214efc36f5ddbcd1f16348a84e9
bonheur15 <rumanzibonheur@gmail.com>
2025-06-11 14:13:33 +0200

    refactor(game): implement dynamic import for PokemonFlipGame component fix: update socketpush-web dependency to version 0.2.5

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import React from "react"; // Required for JSX if not already there
-import PokemonFlipGame from "./game";
+import dynamic from "next/dynamic";
+import React from "react";
 
-export default function GomePage() {
+const PokemonFlipGame = dynamic(() => import("./game"), {
+  loading: () => <p>Loading...</p>, // Optional: fallback while loading
+  ssr: false, // Optional: skip server-side rendering
+});
+
+export default function GomePage({
+  searchParams,
+}: {
+  searchParams: { page: number; size: number };
+}) {
   return <PokemonFlipGame />;
 }
-export const dynamic = "force-dynamic";
